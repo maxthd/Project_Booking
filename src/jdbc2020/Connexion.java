fca0e9f3f9ab7f2aaae82dd1ae625d3b2f4ec34e
Azizo27 <78748936+Azizo27@users.noreply.github.com>
2022-03-20 00:20:14 +0100

    Template Connexion BDD + Update

---
+++ src/jdbc2020/Connexion.java
@@ -0,0 +1,119 @@
+
+package jdbc2020;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+public class Connexion {
+
+    private final Connection conn;
+    private final Statement stmt;
+    private ResultSet rset;
+    private ResultSetMetaData rsetMeta;
+
+    public ArrayList<String> tables = new ArrayList<>();
+
+    public ArrayList<String> requetes = new ArrayList<>();
+
+
+    /**Constructeur */
+    public Connexion(String nameDatabase, String loginDatabase, String passwordDatabase) throws SQLException, ClassNotFoundException{
+        // chargement driver "com.mysql.jdbc.Driver"
+        Class.forName("com.mysql.jdbc.Driver");
+        String urlDatabase = "jdbc:mysql://localhost:3306/" + nameDatabase;
+
+
+
+        //création d'une connexion JDBC à la base
+        conn = DriverManager.getConnection(urlDatabase, loginDatabase, passwordDatabase);
+
+        // création d'un ordre SQL (statement)
+        stmt = conn.createStatement();
+    }
+
+
+
+    public void ajouterTable(String table) {
+        tables.add(table);
+    }
+
+
+    /**Ajout d'une ligne dans Requete (3ème colonne)*/
+    public void ajouterRequete(String requete) {
+        requetes.add(requete);
+    }
+
+
+    public ArrayList remplirChampsTable(String table) throws SQLException {
+        // récupération de l'ordre de la requete
+        rset = stmt.executeQuery("SELECT * FROM " + table);
+
+        // récupération du résultat de l'ordre
+        rsetMeta = rset.getMetaData();
+
+        // calcul du nombre de colonnes du resultat
+        int nbColonne = rsetMeta.getColumnCount();
+
+        // creation d'une ArrayList de String
+        ArrayList<String> liste;
+        liste = new ArrayList<>();
+        String champs = "";
+        // Ajouter tous les champs du resultat dans l'ArrayList
+        for (int i = 0; i < nbColonne; i++) {
+            champs = champs + " " + rsetMeta.getColumnLabel(i + 1);
+        }
+
+        // ajouter un "\n" à la ligne des champs
+        champs = champs + "\n";
+
+        // ajouter les champs de la ligne dans l'ArrayList
+        liste.add(champs);
+
+        // Retourner l'ArrayList
+        return liste;
+    }
+
+
+    public ArrayList remplirChampsRequete(String requete) throws SQLException {
+        // récupération de l'ordre de la requete
+        rset = stmt.executeQuery(requete);
+
+        // récupération du résultat de l'ordre
+        rsetMeta = rset.getMetaData();
+
+        // calcul du nombre de colonnes du resultat
+        int nbColonne = rsetMeta.getColumnCount();
+
+        // creation d'une ArrayList de String
+        ArrayList<String> liste;
+        liste = new ArrayList<>();
+
+        // tant qu'il reste une ligne
+        while (rset.next()) {
+            String champs;
+            champs = rset.getString(1); // ajouter premier champ
+
+            // Concatener les champs de la ligne separes par ,
+            for (int i = 1; i < nbColonne; i++) {
+                champs = champs + "," + rset.getString(i + 1);
+            }
+
+            // ajouter un "\n" à la ligne des champs
+            champs = champs + "\n";
+
+            // ajouter les champs de la ligne dans l'ArrayList
+            liste.add(champs);
+        }
+
+        // Retourner l'ArrayList
+        return liste;
+    }
+
+
+    public void executeUpdate(String requeteMaj) throws SQLException {
+        stmt.executeUpdate(requeteMaj);
+    }
+
+
+}
---
+++ src/jdbc2020/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Code JDBC de Mr. SEGADO
+//https://stackoverflow.com/questions/27053276/date-and-time-picker-in-java
+package jdbc2020;
+
+
+public class Main {
+
+    public static void main(String[] s) {
+
+        Fenetre f = new Fenetre();
+    }
+}
